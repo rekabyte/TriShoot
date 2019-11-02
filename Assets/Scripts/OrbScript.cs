15ecd5ad5a95128d41f2b77d8a104cd8e443a6fa
rekabyte <khalilplaybox@outlook.fr>
2019-11-02 12:50:11 +0100

    First clonage

---
+++ Assets/Scripts/OrbScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbScript : MonoBehaviour
+{
+    public float orbForce;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddRelativeForce(Vector2.up * orbForce);
+        StartCoroutine(DestructCo());
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Tile")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestructCo()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, shootDelay, dashDelay, dashForce, dashTime;
+
+    public GameObject orb;
+    public GameObject shotPoint, trailPoint;
+
+    private Rigidbody2D rb;
+    private bool canShoot, canDash;
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        canShoot = true;
+        canDash = true;
+        trailPoint.gameObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        LookAt();
+        Move();
+        Shoot();
+        Dash();
+    }
+
+    void LookAt()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void Move()
+    {
+        Vector3 kbInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.position+= kbInput * speed * Time.deltaTime;
+    }
+
+    void Dash()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(DashCo());
+            rb.AddForce(transform.up * dashForce, ForceMode2D.Impulse);
+            StartCoroutine(StopCo(dashTime));
+        }
+    }
+
+    void Shoot()
+    {
+        if(canShoot)
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                StartCoroutine(ShootCo());
+                Instantiate(orb, shotPoint.transform.position, this.gameObject.transform.rotation);
+            }
+        }
+    }
+
+    IEnumerator ShootCo()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootDelay);
+        canShoot = true;
+    }
+
+    IEnumerator DashCo()
+    {
+        canDash = false;
+        trailPoint.gameObject.SetActive(true);
+        
+        yield return new WaitForSeconds(dashDelay);
+        
+        canDash = true;
+    }
+
+    IEnumerator StopCo(float dashTime)
+    {
+        yield return new WaitForSeconds(this.dashTime);
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(2.0f);
+        trailPoint.gameObject.SetActive(false);
+    }
+
+
+}
